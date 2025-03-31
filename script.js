263e65ae24fb348cbc0dd2c4b4f7a28d003159f7
RilyRobo <rilyrobo@gmail.com>
2025-03-31 01:07:42 -0700

    Add About Me page and update navigation; remove unused commission icon

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const galleryData = [
         {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/31357645/featured', title: 'Featured', icon: 'images/icon/nav_icon_Work.gif', description: 'A collection of my latest work'},
-        {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/96210416/2d-art', title: '2D Art', icon: 'images/icon/nav_icon_Work.gif', description: 'A collection of my 2D artwork'},
+        {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/96210416/2d-art', title: '2D Art', icon: 'images/icon/nav_icon_Gallery.gif', description: 'A collection of my 2D artwork'},
         {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/96210354/3d-art', title: '3D Art', icon: 'images/icon/nav_icon_3D.gif', description: 'A collection of my 3D artwork'},
-        {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/57218144/reference-images', title: 'Character Design', icon: 'images/icon/nav_icon_Work.gif', description: 'A collection of my character designs'},
+        {url: 'https://backend.deviantart.com/rss.xml?q=gallery:RilyRobo/57218144/reference-images', title: 'Character Design', icon: 'images/icon/nav_icon_Gallery2.gif', description: 'A collection of my character designs'},
     ];
     
     fetchGalleryData(galleryData[0], '.gallery-grid-home', 5);
@@ -25,22 +25,76 @@ document.addEventListener("DOMContentLoaded", () => {
     setupGalleries(galleryData);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const username = "RilyRobo";
-    const platforms = [
-        { name: "Twitter", url: `https://twitter.com/${username}`, icon: "https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=ffffff" , color: "#1A91DA" },
-        { name: "Instagram", url: `https://instagram.com/${username}`, icon: "https://img.icons8.com/?size=100&id=32309&format=png&color=ffffff" , color: "#C13584" },
-        { name: "YouTube", url: `https://www.youtube.com/@${username}`, icon: "https://img.icons8.com/?size=100&id=37326&format=png&color=ffffff" , color: "#CC0000" },
-        { name: "DeviantArt", url: `https://www.deviantart.com/${username}`, icon: "https://img.icons8.com/?size=100&id=38504&format=png&color=ffffff" , color: "#04A045" },
-        { name: "Artstation", url: `https://www.artstation.com/${username}`, icon: "https://img.icons8.com/?size=100&id=pB77uEobJRjy&format=png&color=ffffff" , color: "#42A5F5FF" },
-        { name: "Newgrounds", url: `https://${username}.newgrounds.com`, icon: "https://img.icons8.com/?size=100&id=15771&format=png&color=ffffff" , color: "#E6B800" },
-        { name: "Twitch", url: `https://www.twitch.tv/${username}`, icon: "https://img.icons8.com/?size=100&id=18104&format=png&color=ffffff" , color: "#772CE8" },
-        { name: "Picarto", url: `https://picarto.tv/${username}`, icon: "https://img.icons8.com/?size=100&id=6byL4WgkpyPg&format=png&color=ffffff" , color: "#00CC00" },
-        { name: "Patreon", url: `https://www.patreon.com/c/${username}`, icon: "https://img.icons8.com/?size=100&id=tIshI0hyXw3f&format=png&color=ffffff" , color: "#E36254" },
-        { name: "Subscribestar", url: `https://www.subscribestar.com/${username}`, icon: "https://img.icons8.com/?size=100&id=7856&format=png&color=ffffff" , color: "#E6B800" },
-        { name: "Ko-fi", url: `https://www.ko-fi.com/${username}`, icon: "https://img.icons8.com/?size=100&id=8342&format=png&color=ffffff" , color: "#E6C200" },
-    ];
+const username = "RilyRobo";
+const platforms = [
+    { name: "Twitter", url: `https://twitter.com/${username}`, icon: "https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=ffffff" , color: "#1A91DA" },
+    { name: "Instagram", url: `https://instagram.com/${username}`, icon: "https://img.icons8.com/?size=100&id=32309&format=png&color=ffffff" , color: "#C13584" },
+    { name: "YouTube", url: `https://www.youtube.com/@${username}`, icon: "https://img.icons8.com/?size=100&id=37326&format=png&color=ffffff" , color: "#CC0000" },
+    { name: "DeviantArt", url: `https://www.deviantart.com/${username}`, icon: "https://img.icons8.com/?size=100&id=38504&format=png&color=ffffff" , color: "#04A045" },
+    { name: "Artstation", url: `https://www.artstation.com/${username}`, icon: "https://img.icons8.com/?size=100&id=pB77uEobJRjy&format=png&color=ffffff" , color: "#42A5F5FF" },
+    { name: "Newgrounds", url: `https://${username}.newgrounds.com`, icon: "https://img.icons8.com/?size=100&id=15771&format=png&color=ffffff" , color: "#E6B800" },
+    { name: "Twitch", url: `https://www.twitch.tv/${username}`, icon: "https://img.icons8.com/?size=100&id=18104&format=png&color=ffffff" , color: "#772CE8" },
+    { name: "Picarto", url: `https://picarto.tv/${username}`, icon: "https://img.icons8.com/?size=100&id=6byL4WgkpyPg&format=png&color=ffffff" , color: "#00CC00" },
+    { name: "Patreon", url: `https://www.patreon.com/c/${username}`, icon: "https://img.icons8.com/?size=100&id=tIshI0hyXw3f&format=png&color=ffffff" , color: "#E36254" },
+    { name: "Subscribestar", url: `https://www.subscribestar.com/${username}`, icon: "https://img.icons8.com/?size=100&id=7856&format=png&color=ffffff" , color: "#E6B800" },
+    { name: "Ko-fi", url: `https://www.ko-fi.com/${username}`, icon: "https://img.icons8.com/?size=100&id=8342&format=png&color=ffffff" , color: "#E6C200" },
+];
 
+const contacts = [
+    { name: "Twitter", url: `https://twitter.com/${username}`, icon: "https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=ffffff" , color: "#1A91DA" },
+    { name: "Ko-fi", url: `https://www.ko-fi.com/${username}`, icon: "https://img.icons8.com/?size=100&id=8342&format=png&color=ffffff" , color: "#E6C200" },
+    { name: "Email", url: `mailto:${username}@gmail.com`, icon: "https://img.icons8.com/?size=100&id=60688&format=png&color=ffffff" , color: "#ffffff" },
+    { name: "Discord", url: `https://discordapp.com/users/277498825403531264`, icon: "https://img.icons8.com/?size=100&id=30888&format=png&color=ffffff" , color: "#7289da" },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const socialMediasContainers = document.querySelectorAll(".social-contact");
+
+    if (socialMediasContainers.length === 0) {
+        console.error("No containers with class 'social-contact' found.");
+        return;
+    }
+
+    socialMediasContainers.forEach(container => {
+        contacts.forEach(contact => {
+            const link = document.createElement("a");
+            link.href = contact.url;
+            link.target = "_blank";
+            link.rel = "noopener noreferrer";
+            link.addEventListener("click", (e) => e.stopPropagation());
+
+            const imgStart = document.createElement("img");
+            imgStart.src = contact.icon;
+            imgStart.alt = contact.name;
+            imgStart.classList.add("social-icon");
+
+            const imgEnd = imgStart.cloneNode(true);
+
+            if (container.classList.contains("social-contact-icon")) {
+                link.appendChild(imgStart);
+            } else {
+                link.innerHTML = `<strong>${platform.name}</strong>`;
+                link.style.display = "flex";
+                link.style.alignItems = "center";
+                link.style.justifyContent = "space-between";
+                link.style.marginBottom = "10px";
+                link.style.textDecoration = "none";
+                link.style.color = "#ffffff";
+                link.style.fontSize = "16px";
+                link.style.backgroundColor = platform.color;
+                link.style.padding = "5px 10px";
+                link.style.borderRadius = "8px";
+                link.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)";
+                link.insertBefore(imgStart, link.firstChild);
+                link.appendChild(imgEnd);
+            }
+
+            container.appendChild(link);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
     const socialMediasContainers = document.querySelectorAll(".social-media");
 
     if (socialMediasContainers.length === 0) {
@@ -1028,3 +1082,30 @@ function toggleMobileNav() {
 function ScrollToTop() {
     window.scrollTo(0,0);
 }
+
+function loadAboutContent() {
+    const aboutContentElement = document.getElementById('about-content');
+
+    if (!aboutContentElement) {
+        console.error('Element with ID "about-content" not found.');
+        return;
+    }
+
+    fetch('aboutme.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to load aboutme.html: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            aboutContentElement.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading aboutme.html:', error);
+            aboutContentElement.innerHTML = '<p>Failed to load content. Please try again later.</p>';
+        });
+}
+
+// Call the function when the page loads
+document.addEventListener('DOMContentLoaded', loadAboutContent);
